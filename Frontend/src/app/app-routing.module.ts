c80bfc4e44f469829c8dca07716d2a1e99272a55
Sarah Lisboa <SarahLisboa@iheartmedia.com>
2020-03-23 23:02:11 -0300

    fixing sign-in and sign-up

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { LoggedComponent } from './logged/logged.component';
+import { SignInComponent } from './login/sign-in/sign-in.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
   {path: 'logged', component: LoggedComponent}
 ];
 
